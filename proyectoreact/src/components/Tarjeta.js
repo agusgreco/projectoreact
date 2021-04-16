8b7b729e9f64f677c880a5d868df1d9d761580f4
agusgreco <49990145+agusgreco@users.noreply.github.com>
2021-04-16 18:23:41 -0400

    api

--- proyectoreact/src/components/Tarjeta.js
+++ proyectoreact/src/components/Tarjeta.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 
 class Tarjeta extends Component {
-    // constructor(){
-    //     super();
-    //     this.state ={
-    //     }
-    //   }
+    constructor(){
+        super();
+        this.state ={
+        }
+      }
+
+
+    componentDidMount(){
+        console.log("Se creo la tarjeta "+ this.props.id);
+      }
+    
+      componentDidUpdate(prevProps, prevStates){
+        console.log("Se actualizo la tarjeta "+ this.props.id);
+        console.log(prevProps);
+        console.log(prevStates)
+    
+      }
+    
+      componentWillUnmount(){
+        console.log("Se borro la tarjeta "+ this.props.id);
+      }
+
+      verDetalle(){
+          
+      }
    
    render(){
     return (
@@ -13,11 +33,12 @@ class Tarjeta extends Component {
 
                     <div className="tarjeta-text">
                         <div className="image"> 
-                          <img  src={props.tarjetaAMostrar.image}/> 
+                          <img  src={props.tarjetaAMostrar.picture.medium}/> 
                         </div>
-                        <div className="nombreyapellido words"> {props.tarjetaAMostrar.nombreyapellido} </div>
+                        <div className="nombreyapellido words"> {props.tarjetaAMostrar.name.first} {props.tarjetaAMostrar.name.last} </div>
                         <div className="email words"> {props.tarjetaAMostrar.email}</div>
-                        <div className="nacimientoyedad words">{props.tarjetaAMostrar.nacimientoyedad} </div>
+                        <div className="nacimientoyedad words">{props.tarjetaAMostrar.dob.date} - {props.tarjetaAMostrar.dob.age} ys </div>
+                        <button type="button"onClick={this.props.verDetalle.bind(this, this.props.tarjetaAMostrar.id)}>Ver detalle</button>
                         <button type="button" onClick={this.props.onBorrar.bind(this, this.props.tarjetaAMostrar.id)}>Borrar</button>
                     </div>
 
--- proyectoreact/src/components/Tira.js
+++ proyectoreact/src/components/Tira.js
@@ -1,34 +1,44 @@
 import Tarjeta from './Tarjeta';
 import {Component} from 'react';
 
-const arrayTarjetas = [
-    {
-        nombreyapellido:"Agus",
-        email:"@mail",
-        nacimientoyedad: "1/3/02"
-    },
-    {
-        nombreyapellido:"Emma", 
-        email:"@mail", 
-        nacimientoyedad:"1/4/02"   
-    },
-    {
-        nombreyapellido:"Kane",
-        email:"@mail", 
-        nacimientoyedad:"1/3/02"
-    }
-]
+// const arrayTarjetas = [
+//     {
+//         nombreyapellido:"Agus",
+//         email:"@mail",
+//         nacimientoyedad: "1/3/02"
+//     },
+//     {
+//         nombreyapellido:"Emma", 
+//         email:"@mail", 
+//         nacimientoyedad:"1/4/02"   
+//     },
+//     {
+//         nombreyapellido:"Kane",
+//         email:"@mail", 
+//         nacimientoyedad:"1/3/02"
+//     }
+// ]
 
 class Tira extends Component{
 
     constructor(){
         super();
         this.state ={
-            datos: arrayTarjetas,
+            datos: [],
         }
      }  
 
 
+     componentDidMount(){
+        fetch("https://randomuser.me/api/?results=10")
+        .then(result => result.json())
+        .then(data => {
+          this.setState({datos: data.results})
+          console.log(data)
+        })
+      }
+
+      
      borrarItem(idTarjeta){
         let resultado = this.state.datos.filter( (dato) => {
           return dato.id !== idTarjeta
@@ -37,10 +47,30 @@ class Tira extends Component{
         console.log(this.state.datos)
      }
 
+     agregarTarjetas(){
+        console.log("se agregaron 10 tarjetas")
+        fetch("https://randomuser.me/api/?results=10")
+        .then(result => result.json())
+        .then(data => {
+          this.state.datos.push(data.results);
+          this.setState({datos: this.state.datos})
+        })
+      }
+
+      filtrarTarjetas(){
+
+      }
+
+
      render(){
       return (
         <div className="rowbody">
-            {this.state.datos.map((unaTarjeta, idx) =>(
+           <button onClick={this.agregarTarjetas.bind(this)}>Agregar Tarjetas</button>
+           <div>
+                {/* <input type="search" placeholder="Search" aria-label="Search"> */}
+                <button onClick={this.filtrarTarjetas.bind(this)}>Filtrar</button>
+            </div>
+           {this.state.datos.map((unaTarjeta, idx) =>(
                 <div className="unaTarjeta" key={idx}>
                   <Tarjeta tarjetaAMostrar={unaTarjeta}  onBorrar={this.borrarItem.bind(this)}/>
                 </div>
