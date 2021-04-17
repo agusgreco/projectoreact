4d1c27487c1e5a9ccc243a081bb2a5f265a77608
ema <ejuarez@udesa.edu.ar>
2021-04-17 20:15:55 -0300

    css

--- proyectoreact/src/components/Header.js
+++ proyectoreact/src/components/Header.js
@@ -5,7 +5,7 @@ function Header(params) {
                 {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button> */}
-                <h1>LIBRO DE CARAS</h1>
+                <h1 className="titulo">LIBRO DE CARAS</h1>
             </nav>
 
         </header>
