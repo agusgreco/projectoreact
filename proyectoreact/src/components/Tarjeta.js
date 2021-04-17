a5c69d554b31bc78286996952e44242a42938165
ema <ejuarez@udesa.edu.ar>
2021-04-17 20:38:24 -0300

    cambios css

--- proyectoreact/src/components/Tarjeta.js
+++ proyectoreact/src/components/Tarjeta.js
@@ -49,6 +49,7 @@ class Tarjeta extends Component {
                         <div className="nombreyapellido words"> {this.props.tarjetaAMostrar.name.first} {this.props.tarjetaAMostrar.name.last} </div>
                         <div className="email words"> {this.props.tarjetaAMostrar.email}</div>
                         <div className="nacimientoyedad words">{this.props.tarjetaAMostrar.dob.date} - {this.props.tarjetaAMostrar.dob.age} ys </div>
+<<<<<<< Updated upstream
                         
                         {/* <div className="location words">{this.props.tarjetaAMostrar.location.street} {this.props.tarjetaAMostrar.location.city} {this.props.tarjetaAMostrar.location.state} {this.props.tarjetaAMostrar.location.postcode}</div> */}
                         {/* <div className="fecharegistro words">Fecha de registro: {this.props.tarjetaAMostrar.registered.date}</div> */}
@@ -57,6 +58,10 @@ class Tarjeta extends Component {
 
                         {/* <button type="button" onClick={this.props.verDetalle.bind(this, this.props.tarjetaAMostrar.id)}>Ver detalle</button> */}
                         <button type="button" onClick={this.props.onBorrar.bind(this, this.props.tarjetaAMostrar.id)}>Borrar</button>
+=======
+                        {/* <button type="button"onClick={this.props.verDetalle.bind(this, this.props.tarjetaAMostrar.id)}>Ver detalle</button> */}
+                        <button type="button" className="borrar" onClick={this.props.onBorrar.bind(this, this.props.tarjetaAMostrar.id)}>Borrar</button>
+>>>>>>> Stashed changes
                     </div>
 
         </div>
