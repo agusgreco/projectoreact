f694b69f81e184a0784ee00add577870207c1d9f
agusgreco <49990145+agusgreco@users.noreply.github.com>
2021-04-23 14:40:06 -0400

    cambiando detalles

--- proyectoreact/src/components/Tarjeta.js
+++ proyectoreact/src/components/Tarjeta.js
@@ -27,7 +27,7 @@ class Tarjeta extends Component {
         console.log("Se borro la tarjeta "+ this.props.id);
       }
 
-      verDetalle(tamano){
+      verMas(tamano){
         // if(this.state.tamanoOriginal !== "50%"){
           this.setState({
             tamanoNuevo: tamano,
@@ -36,7 +36,7 @@ class Tarjeta extends Component {
         // }
       }
 
-      verMas(visibility){
+      verDetalle(visibility){
           this.setState({
             visibilityNuevo: visibility,
           });
@@ -47,7 +47,7 @@ class Tarjeta extends Component {
    
    render(){
     return (
-      <div  className="tarjeta-text" style={{width: this.state.tamanoNuevo}} onMouseEnter={ () => this.verDetalle("50%")} onMouseLeave={ () => this.verDetalle(this.state.tamanoOriginal)}>
+      <div  className="tarjeta-text" style={{width: this.state.tamanoNuevo}} onMouseEnter={ () => this.verMas("50%")} onMouseLeave={ () => this.verMas(this.state.tamanoOriginal)}>
 
 
                       <div className="x">
@@ -64,10 +64,10 @@ class Tarjeta extends Component {
                         <div className="nacimientoyedad words">{this.props.tarjetaAMostrar.dob.date} - {this.props.tarjetaAMostrar.dob.age} ys </div>
                         
 
-                   {/* <button type="button" onClick={ () => this.verMas("block")}>Ver detalle</button>
+                   {/* <button type="button" onClick={ () => this.verDetalle("block")}>Ver detalle</button>
                    <div  style={{display: this.state.visibilityNuevo}} 
-                  //  onClick={ () => this.verMas("block")}
-                  //  onMouseLeave={ () => this.verMas(this.state.visibilityOriginal)}
+                  //  onClick={ () => this.verDetalle("block")}
+                  //  onMouseLeave={ () => this.verDetalle(this.state.visibilityOriginal)}
                    >
                         <div className="location words">{this.props.tarjetaAMostrar.location.street} {this.props.tarjetaAMostrar.location.city} {this.props.tarjetaAMostrar.location.state} {this.props.tarjetaAMostrar.location.postcode}</div>
                         <div className="fecharegistro words">Fecha de registro: {this.props.tarjetaAMostrar.registered.date}</div>
