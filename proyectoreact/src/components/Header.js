daf4e45cfe01197c4ba4454de2fedd9127834f26
ema <ejuarez@udesa.edu.ar>
2021-04-30 16:31:35 -0300

    saque coments

--- proyectoreact/src/components/Header.js
+++ proyectoreact/src/components/Header.js
@@ -2,12 +2,8 @@ function Header(params) {
     return (
         <header >
             <nav id="header" className="ache1 sticky">
-                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button> */}
                 <h1 className="titulo">LIBRO DE CARAS</h1>
             </nav>
-
         </header>
     )
 }
